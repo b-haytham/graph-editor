e7c170da2c3f88d550207fc359c79d73267d5256
haytham boussarsar <boussarsarhaytham@gmail.com>
2022-11-07 15:06:15 +0100

    [web] fix canvas resize

--- apps/graph-editor-web/app/page.tsx
+++ apps/graph-editor-web/app/page.tsx
@@ -2,6 +2,14 @@ import Editor from "editor/Editor";
 
 export default function Home() {
   return (
-    <Editor />
-  )
+    <div className="h-screen w-screen flex flex-col">
+      {/*<section className="w-[200px] bg-cyan-100">
+        <div className="bg-black h-10" />
+      </section>*/}
+
+      <div className="flex-grow h-full">
+        <Editor zoomControl />
+      </div>
+    </div>
+  );
 }
